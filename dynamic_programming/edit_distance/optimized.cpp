79db057b9a4305a6ca6dfc1c8d29c5edc9d29d95
Subhojit Dey <subhojit2000dey@gmail.com>
2026-05-30 08:27:54 +0530

    CSES | DP-10 | Edit Distance

--- dynamic_programming/edit_distance/optimized.cpp
+++ dynamic_programming/edit_distance/optimized.cpp
@@ -15,8 +15,8 @@ int main(){
     int n = a.size(), m = b.size();
 
     vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-    for(int j = 0; j <= m; j++) dp[0][j] = j;
-    for(int i = 0; i <= n; i++) dp[i][0] = i;
+    for(int j = 0; j <= m; j++) dp[0][j] = j; // base cases
+    for(int i = 0; i <= n; i++) dp[i][0] = i; // base cases
 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= m; j++){
