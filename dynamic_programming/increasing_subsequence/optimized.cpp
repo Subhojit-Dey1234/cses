1fd55b93afcdd075ea8b53a25112812f70d5a573
Subhojit Dey <subhojit2000dey@gmail.com>
2026-06-21 22:12:37 +0530

    Increasing Subsequence

---
+++ dynamic_programming/increasing_subsequence/optimized.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include<unordered_map>
+#include<unordered_set>
+#include <climits>
+#include <algorithm>
+#include <string>
+#define ll long long
+
+using namespace std;
+
+int main(){
+    ll n;
+    cin >> n;
+    vector<ll> dp;
+    for(ll i = 0; i < n; i++){
+        ll x;
+        cin >> x;
+        auto it = upper_bound(dp.begin(), dp.end(), x);
+
+        if(it == dp.end()) dp.push_back(x);
+        else *it = x;
+    }
+
+    cout << dp.size() << endl;
+}
