929f5b5f4db1559f6067eab1166589081c650244
Subhojit Dey <subhojit2000dey@gmail.com>
2026-06-27 20:09:57 +0530

    Adding my index.md

---
+++ dynamic_programming/elevator_rides/brute_force.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include<unordered_map>
+#include<unordered_set>
+#include <climits>
+#include <algorithm>
+#include <string>
+#define ll long long
+
+using namespace std;
+
+int main(){
+    ll n, x;
+    vector<ll> wt(n);
+    for(ll& w : wt) cin >> w;
+
+
+}
